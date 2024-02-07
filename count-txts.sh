dbedc84c5b9e2ce27f689dee2c2dc2e5b5f76839
anikaagarwal12 <147211541+anikaagarwal12@users.noreply.github.com>
2024-02-07 11:27:44 -0800

    Add files via upload

---
+++ count-txts.sh
@@ -0,0 +1,9 @@
+find $1 > find-results.txt
+grep ".txt" find-results.txt > grep-results.txt
+xargs wc grep-results.txt 
+
+ #sort results.txt
+
+# find technical > find-results.txt
+# grep ".txt" find-results.txt > grep-results.txt
+# wc grep-results.txt
---
+++ start.sh
@@ -0,0 +1,3 @@
+javac Server.java DocSearchServer.java
+java DocSearchServer 3020 technical
+
